92be4b42d4b1542211a5544a9eb7189120e35ff0
sushil <sushil@nvestbank.com>
2019-01-24 16:49:17 +0530

    more navigation added and javasrcript code added

---
+++ static/js/test.js
@@ -0,0 +1,36 @@
+I("div", {
+    staticClass: "menu__item"
+}, [I("div", {
+    staticClass: "menu__item-bg menu__item-bg--9"
+}), I("div", {
+    staticClass: "menu__item-bg menu__item-bg-cover menu__item-bg--9"
+}), I("a", {
+    staticClass: "menu__link menu__item-content",
+    attrs: {
+        href: "https://terminal.globalxchange.com/",
+        target: "_blank"
+    },
+    on: {
+        click: function(B) {
+            A.setMenuAction(!1)
+        }
+    }
+}, [I("img", {
+    staticClass: "menu__item-img menu__item-img--2",
+    attrs: {
+        src: C("N4M5")
+    }
+}), I("div", {
+    staticClass: "menu__item-inner"
+}, [I("h3", {
+    staticClass: "menu__item-title"
+}, [A._v("Terminals")]), I("div", {
+    staticClass: "menu__item-descContainer"
+}, [I("icon", {
+    staticClass: "menu__icon menu__icon--separator",
+    attrs: {
+        name: "icon-separator"
+    }
+}), I("span", {
+    staticClass: "menu__item-desc"
+}, [A._v("terminal.globalxchange.com")])], 1)])])]), 
