11460ad23fef7af13f2165e3e8ae20e56ee192e2
sushil <sushil@nvestbank.com>
2019-01-28 11:14:39 +0530

    content made responsive according to resolution

---
+++ static/js/test.js
@@ -0,0 +1,44 @@
+
+                I("div", {
+                    staticClass: "menu__item"
+                }, [I("div", {
+                    staticClass: "menu__item-bg menu__item-bg--5"
+                }), I("a", {
+                    staticClass: "menu__link menu__item-content",
+                    attrs: {
+                        href: "https://auth.gtx.globalxchange.com/accounts/sign_up",
+                        target: "_BLANK"
+                    },
+                    nativeOn: {
+                        click: function(B) {
+                            A.setMenuAction(!1)
+                        }
+                    }
+                }, [I("div", {
+                    staticClass: "menu__item-inner"
+                }, [I("h3", {
+                    staticClass: "menu__item-title"
+                }, [A._v("ifo")]), I("div", {
+                    staticClass: "menu__item-descContainer menu__item-descContainer-icon"
+                })])])]), I("div", {
+                    staticClass: "menu__item"
+                }, [I("div", {
+                    staticClass: "menu__item-bg menu__item-bg--6"
+                }), I("a", {
+                    staticClass: "menu__link menu__item-content",
+                    attrs: {
+                        href: "https://broker1.globalxchangetoken.com",
+                        target: "_BLANK"
+                    },
+                    nativeOn: {
+                        click: function(B) {
+                            A.setMenuAction(!1)
+                        }
+                    }
+                }, [I("div", {
+                    staticClass: "menu__item-inner"
+                }, [I("h3", {
+                    staticClass: "menu__item-title"
+                }, [A._v("NVEST")]), I("div", {
+                    staticClass: "menu__item-descContainer menu__item-descContainer-icon"
+                })])])]), 
